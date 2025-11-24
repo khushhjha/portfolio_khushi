0370236c71b9261d23e296d52b297ff22641d9d3
khushi jha <khushijha671@gmail.com>
2025-11-25 01:11:26 +0530

    Clean project structure

--- portfolio_Khushi/script.js
+++
@@ -1,9 +0,0 @@
-
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click', function(e){
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if(target) target.scrollIntoView({behavior:'smooth', block:'start'});
-  });
-});
